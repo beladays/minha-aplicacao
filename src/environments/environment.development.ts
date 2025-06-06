523007de8b59630e59841078a27af7f7dfddbb75
Isabela <isabeladays63@gmail.com>
2025-06-06 01:15:43 -0300

    Clonei api de back-end, adicionei coluna descrição , criei serviço

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,4 @@
+// src\environments\environment.development.ts
+export const environment = {
+api: 'http://localhost:3000/'
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+// src\environments\environment.ts
+export const environment = {
+api: 'http://myawesomesite.com:5461/api/'
+}
